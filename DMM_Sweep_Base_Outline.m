a78d239c88d6ec3a28d80f173d229ebbc6d98c3e
Dakota Jameson <kotacjameson@gmail.com>
2024-12-19 20:04:40 -0600

    Add files via upload
    This is the skeletal structure I used to create the voltage-sweeping program. It was created through research and the assistance of lab teaching assistants.

---
+++ DMM_Sweep_Base_Outline.m
@@ -0,0 +1,21 @@
+% Create a VISA object
+dmm = visa('keysight', 'USB0::0x2A8D::0x8E01::CN63470074::0::INSTR');
+
+% Opening the connection to the DMM
+fopen(dmm);
+
+% Configure the measurement on the DMM
+fprintf(dmm, 'MEASURE:VOLT:DC? AUTO,MAX');
+
+% Storing DMM values as a double
+volt = str2double(fscanf(dmm));
+
+% Displaying the measured values
+disp(['Measurement_Result:,' measurement]);
+
+% Closing the connection
+fclose(dmm);
+
+% Clear the memory
+delete(dmm);
+clear dmm;
